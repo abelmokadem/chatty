ef012ab6551b5d15864648369a3a75dba2a76f61
Ash Belmokadem <a.belmokadem@gmail.com>
2016-12-20 18:07:47 +0000

    Create segment component

--- web/src/app/components/components.module.ts
+++ web/src/app/components/components.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SegmentComponent } from './segment/segment.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [
-
+    SegmentComponent
+  ],
+  exports: [
+    SegmentComponent
   ]
 })
 export class ComponentsModule { }
---
+++ web/src/app/components/segment/segment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'components-segment',
+  templateUrl: './segment.component.html',
+  styleUrls: ['./segment.component.scss']
+})
+export class SegmentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
